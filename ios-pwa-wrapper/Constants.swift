0554c5baf79c0108723c43c1a8c73972513fa1aa
Mark Chiles <mechiles@mccomps.com>
2021-01-08 18:31:30 -0600

    updates

--- ios-pwa-wrapper/Constants.swift
+++ ios-pwa-wrapper/Constants.swift
@@ -9,9 +9,9 @@
 import UIKit
 
 // Basic App-/WebView-configuration
-let appTitle = "iOS PWA Wrapper"
-let webAppUrl = URL(string: "https://www.leasingrechnen.at")
-let allowedOrigin = "leasingrechnen.at"
+let appTitle = "Fishing Crunch"
+let webAppUrl = URL(string: "https://www.fishingcrunch.com")
+let allowedOrigin = "fishingcrunch.com"
 let menuButtonTitle = NSLocalizedString("menu", comment: "")
 let menuButtonJavascript = """
     $('.button-collapse').sideNav('show');
